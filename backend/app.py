53a45fe20a45cab23fa2ec75c6421b70c001659f
Miky <michaelalula05@gmail.com>
2026-03-16 12:03:11 +0300

    Small improvement

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
-from knowledge_engine.loader import load_documents
-from knowledge_engine.splitter import split_documents
-from knowledge_engine.vectordb import create_vector_db
 from api.chat import router as chat_router
 
 app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
 ]
+
+frontend_url = os.getenv("FRONTEND_URL")
+if frontend_url:
+    origins.append(frontend_url)
 
 app.add_middleware(
     CORSMiddleware,
